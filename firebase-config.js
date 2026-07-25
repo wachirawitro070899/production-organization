4f8ba8eabc3cbbb5ff6986cbbe6780b6c9be74e2
wachirawitro070899 <wachirawit.ro070899@gmail.com>
2026-07-25 14:53:34 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCkl4CBeRSSJ18bGWQJr119CILZBZoKSm4",
+  authDomain: "production-organization.firebaseapp.com",
+  projectId: "production-organization",
+  storageBucket: "production-organization.firebasestorage.app",
+  messagingSenderId: "20879272032",
+  appId: "1:20879272032:web:46a34f1a17c1be1044fb05",
+  measurementId: "G-GFFQYMS3QB"
 };
